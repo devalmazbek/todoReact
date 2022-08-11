8c42c0c3fa0f9db9d2b0315d55bd7d178f715a33
devalmazbek <devalmazbek@gmail.com>
2022-08-11 08:56:14 +0600

    changed to class component, added state done and important

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,23 +1,81 @@
 import React from "react";
+import { Component } from "react";
 
-import './todo-list-item.css'
+import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
-    const style = {
-        color: important ? 'steelblue' : '',
-        fontWeight: important ? 'bold' : '',
+
+export default class TodoListItem extends Component {
+    constructor(){
+        super();
+        this.state = {
+            done: false,
+            important: false,
+        };
     };
 
-    return (
-        <div className="todo-list-item d-flex justify-content-between">
-            <span className="todo-list-items" style={style}>{ label }</span>
-            <div className="todo-list-icon">
-            <button className="btn btn-danger"><i className="bi bi-trash trash"></i></button>
-                <button className="btn btn-success"><i className="bi bi-exclamation-lg important"></i></button> 
-                 
-            </div>
-        </div>
-    );
+    onLabelClick = () => {
+        this.setState(({done}) => {
+            return {
+                done: !done
+            };
+        });
+    };
+
+    onMarkClick = () => {
+        this.setState(({important})=> {
+            return {
+                important: !important,
+            }
+        });
+    }
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
+
+        let todoItemClass = 'todo-list-items'
+        if(done) {
+            todoItemClass += ' done ';
+        }
+
+        if(important) {
+            todoItemClass += ' important';
+        }
+
+        return (
+            <span className="todo-list-item d-flex justify-content-between">
+                <span className={ todoItemClass }
+                onClick={ this.onLabelClick }>{ label }</span>
+                <span className="todo-list-icon">
+                <button className="btn btn-danger">
+                    <i className="bi bi-trash trash"></i>
+                </button>
+                <button className="btn btn-success"
+                onClick={ this.onMarkClick }>
+                    <i className="bi bi-exclamation-lg important"></i>
+                </button> 
+                     
+                </span>
+            </span>
+        );
+    };
 }
 
-export default TodoListItem;
+// const TodoListItem = ({label, important = false}) => {
+//     const style = {
+//         color: important ? 'steelblue' : '',
+//         fontWeight: important ? 'bold' : '',
+//     };
+
+//     return (
+//         <span className="todo-list-item d-flex justify-content-between">
+//             <span className="todo-list-items" style={style}>{ label }</span>
+//             <span className="todo-list-icon">
+//             <button className="btn btn-danger"><i className="bi bi-trash trash"></i></button>
+//                 <button className="btn btn-success"><i className="bi bi-exclamation-lg important"></i></button> 
+                 
+//             </span>
+//         </span>
+//     );
+// }
+
+// export default TodoListItem;
