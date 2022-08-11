e93cde206f213272c7c9489210c305a9c449c862
devalmazbek <devalmazbek@gmail.com>
2022-08-11 08:57:34 +0600

    TodoFilter component changed to class component

--- src/components/todo-filter/todo-filter.js
+++ src/components/todo-filter/todo-filter.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Component } from 'react';
 
 import './todo-filter.css';
 
 
-const TodoStatus = () => {
-    return (
-        <div className="todo-filter btn-group">
-            <button type="button" className="btn btn-outline-secondary">All</button>
-            <button type="button" className="btn btn-outline-secondary">Active</button>
-            <button type="button" className="btn btn-outline-secondary">Done</button>
-        </div>
- 
-    );
+export default class TodoStatus extends Component {
+    render() {
+        return (
+            <div className="todo-filter btn-group">
+                <button type="button" className="btn btn-outline-secondary">All</button>
+                <button type="button" className="btn btn-outline-secondary">Active</button>
+                <button type="button" className="btn btn-outline-secondary">Done</button>
+            </div>
+     
+        );
+    }
 }
-
-export default TodoStatus;
